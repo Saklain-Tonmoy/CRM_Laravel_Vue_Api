582c5241ecff1df2809f79f2aef31ca2c9c0551f
Saklain-Tonmoy <tonmoysaklain@gmail.com>
2021-03-27 00:01:07 +0600

    components file

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Resources\UpcomingResource;
 use App\Http\Resources\TodayTaskResource;
 use App\Models\Upcoming;
 use App\Models\Today;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,4 +26,36 @@ Route::get('/today', function() {
     $today = Today::all();
 
     return TodayTaskResource::collection($today); // this returns selected columns defined in the TodayTaskResource
+});
+
+// Add a new task
+Route::post('/upcoming', function(Request $request) {
+    return Upcoming::create([
+        'title' => $request->title,
+        'taskId' => $request->taskId,
+        'waiting' => $request->waiting,
+
+    ]);
+});
+
+// Delete upcoming task
+Route::delete('/upcoming/{taskId}', function($taskId) {
+    DB::table('upcomings')->where('taskId', $taskId)->delete();
+
+    return 204;
+});
+
+// Today Task
+Route::post('/dailytask', function(Request $request) {
+    return Today::create([
+        'id' => $request->id,
+        'title' => $request->title,
+        'taskId' => $request->taskId
+    ]);
+});
+
+// Delete Today Task
+Route::delete('/dailytask/{taskId}', function($taskId) {
+    DB::table('todays')->where('taskId', $taskId)->delete();
+    return 204;
 });
